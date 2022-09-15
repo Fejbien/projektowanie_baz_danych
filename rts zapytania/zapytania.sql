dca8a6508757115ac9a6c2d856843955da05b02a
fejbien <sucholasfejbien@gmail.com>
2022-09-15 14:35:12 +0200

    dalje

---
+++ rts zapytania/zapytania.sql
@@ -0,0 +1,11 @@
+SELECT * FROM `areas` WHERE 1;
+SELECT * FROM `areas_out_of_area` WHERE 1;
+SELECT * FROM `buildings` WHERE 1;
+SELECT * FROM `buildings_has_warriors` WHERE 1;
+SELECT * FROM `enemies` WHERE 1;
+SELECT * FROM `players` WHERE 1;
+SELECT * FROM `resources_types` WHERE 1;
+SELECT * FROM `stats_and_resources` WHERE 1;
+SELECT * FROM `warriors` WHERE 1;
+SELECT * FROM `workers` WHERE 1;
+SELECT * FROM `workers_has_buildings` WHERE 1;
